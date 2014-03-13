520a4c38daf475fa3caad333b670d40f55e162b4
James Montemagno <james.montemagno@gmail.com>
2014-03-12 23:26:32 -0700

    UPdate to latest mvvmcross and PCL profile
    Now profile 78

---
+++ Backup/Refractored.MvxPlugins.Settings.WindowsPhone/MvxWindowsPhoneSettings.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+
+namespace Refractored.MvxPlugins.Settings.WindowsPhone
+{
+    public class MvxWindowsPhoneSettings : ISettings
+    {
+        static IsolatedStorageSettings Settings { get { return IsolatedStorageSettings.ApplicationSettings; } }
+        private readonly object m_Locker = new object();
+
+        /// <summary>
+        /// Gets the current value or the default that you specify.
+        /// </summary>
+        /// <typeparam name="T">Vaue of t (bool, int, float, long, string)</typeparam>
+        /// <param name="key">Key for settings</param>
+        /// <param name="defaultValue">default value if not set</param>
+        /// <returns>Value or default</returns>
+        public T GetValueOrDefault<T>(string key, T defaultValue = default(T))
+        {
+            T value;
+            lock (m_Locker)
+            {
+                // If the key exists, retrieve the value.
+                if (Settings.Contains(key))
+                {
+                    value = (T)Settings[key];
+                }
+                // Otherwise, use the default value.
+                else
+                {
+                    value = defaultValue;
+                }
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Adds or updates the value 
+        /// </summary>
+        /// <param name="key">Key for settting</param>
+        /// <param name="value">Value to set</param>
+        /// <returns>True of was added or updated and you need to save it.</returns>
+        public bool AddOrUpdateValue(string key, object value)
+        {
+            bool valueChanged = false;
+
+            lock (m_Locker)
+            {
+                // If the key exists
+                if (Settings.Contains(key))
+                {
+
+                    // If the value has changed
+                    if (Settings[key] != value)
+                    {
+                        // Store key new value
+                        Settings[key] = value;
+                        valueChanged = true;
+                    }
+                }
+                // Otherwise create the key.
+                else
+                {
+                    Settings.Add(key, value);
+                    valueChanged = true;
+                }
+            }
+
+            return valueChanged;
+        }
+
+        /// <summary>
+        /// Saves any changes out.
+        /// </summary>
+        public void Save()
+        {
+            lock (m_Locker)
+            {
+                Settings.Save();
+            }
+        }
+
+    }
+}
--- Backup/Refractored.MvxPlugins.Settings.WindowsPhone/Properties/AssemblyInfo.cs
+++ Backup/Refractored.MvxPlugins.Settings.WindowsPhone/Properties/AssemblyInfo.cs
@@ -1,20 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Refractored.MvxPlugins.Settings.Droid")]
+[assembly: AssemblyTitle("Refractored.MvxPlugins.Settings.WindowsPhone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Refractored.MvxPlugins.Settings.Droid")]
+[assembly: AssemblyProduct("Refractored.MvxPlugins.Settings.WindowsPhone")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfc49539-6a6e-4ad5-ae04-500d65b30f20")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -23,12 +30,8 @@ using Android.App;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.2")]
-[assembly: AssemblyFileVersion("1.2.0.2")]
-
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
